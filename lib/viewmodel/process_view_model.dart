0626e92552a2e2d01b37a675f65c1f03e4889341
Yassineka99 <yassine99ka@gmail.com>
2025-05-06 05:24:51 +0200

    all load data of process and sub process finished ( for offline )

--- lib/viewmodel/process_view_model.dart
+++ lib/viewmodel/process_view_model.dart
@@ -7,18 +7,17 @@ import '../repository/process_repository.dart';
 class ProcessViewModel {
   ProcessRepoitory processRepository = ProcessRepoitory();
   Process? process;
-    Future<void> create(
-      String name, int workflowId , int status , int order , int createdby) async {
+  Future<void> create(
+      String name, int workflowId, int status, int order, int createdby) async {
     try {
-      process =
-          await processRepository.createProcess(name, workflowId,status,order,createdby);
-     
+      process = await processRepository.createProcess(
+          name, workflowId, status, order, createdby);
     } catch (e) {
       print('Error creating client: $e');
     }
   }
 
-    Future<Process?> getbyid(String id) async {
+  Future<Process?> getbyid(String id) async {
     try {
       process = await processRepository.getProcessById(id);
       if (process != null) {
@@ -32,22 +31,27 @@ class ProcessViewModel {
   }
 
   Future<List<Process>> getByUserId(int id) async {
-  return await processRepository.getByUserId(id);
-}
-  Future<List<Process>> getByStatusId(int id) async {
-  return await processRepository.getByStatusId(id);
-}
-
-Future<List<Process>> getByWorkflowId(int id) async {
-  try {
-    final processes = await processRepository.getByWorkflowId(id);
-   
-    return processes;
-  } catch (e, stack) {
-    print('Error fetching processes: $e\n$stack');
-    return [];
+    processRepository.syncProcess();
+    return await processRepository.getByUserId(id);
   }
-}
+
+  Future<List<Process>> getByStatusId(int id) async {
+    processRepository.syncProcess();
+    return await processRepository.getByStatusId(id);
+  }
+
+  Future<List<Process>> getByWorkflowId(int id) async {
+    try {
+      processRepository.syncProcess();
+      final processes = await processRepository.getByWorkflowId(id);
+
+      return processes;
+    } catch (e, stack) {
+      print('Error fetching processes: $e\n$stack');
+      return [];
+    }
+  }
+
   Future<void> update(Process updatedProcess) async {
     try {
       process = await processRepository.updateProcess(updatedProcess);
--- lib/viewmodel/sub_process_view_model.dart
+++ lib/viewmodel/sub_process_view_model.dart
@@ -4,18 +4,17 @@ import '../repository/sub_process_repository.dart';
 class SubProcessViewModel {
   SubProcessRepoitory subprocessRepository = SubProcessRepoitory();
   SubProcess? subprocess;
-    Future<void> create(
-      String name, int processId , int status , String message , int assignedto,int createdby) async {
+  Future<void> create(String name, int processId, int status, String message,
+      int assignedto, int createdby) async {
     try {
-      subprocess =
-          await subprocessRepository.createSubProcess(name, processId,status,message,assignedto,createdby);
-     
+      subprocess = await subprocessRepository.createSubProcess(
+          name, processId, status, message, assignedto, createdby);
     } catch (e) {
       print('Error creating client: $e');
     }
   }
 
-    Future<SubProcess?> getbyid(String id) async {
+  Future<SubProcess?> getbyid(String id) async {
     try {
       subprocess = await subprocessRepository.getSubProcessById(id);
       if (subprocess != null) {
@@ -23,46 +22,50 @@ class SubProcessViewModel {
       } else {
         return null;
       }
-    
     } catch (e) {
       print('Error fetching client: $e');
     }
   }
 
   Future<List<SubProcess>> getAll() async {
-  return await subprocessRepository.getAllSubProcesses();
-}
-
-Future<List<SubProcess>> getByProcessId(int id) async {
-  return await subprocessRepository.getByProcessId(id);
-}
-
-Future<List<SubProcess>> getByUserId(int id) async {
-  return await subprocessRepository.getByUserId(id);
-}
-
-Future<List<SubProcess>> getByUserAndProcess(int userId, int processId) async {
-  return await subprocessRepository.getByUserAndProcessId(userId, processId);
-}
-
-Future<void> update(SubProcess subProcess) async {
-  try {
-    subprocess = await subprocessRepository.updateSubProcess(subProcess);
-  } catch (e) {
-    print('Error updating subprocess: $e');
+    await subprocessRepository.syncSubProcess();
+    return await subprocessRepository.getAllSubProcesses();
   }
-}
 
-Future<void> delete(int id) async {
-  try {
-    await subprocessRepository.deleteSubProcess(id);
-  } catch (e) {
-    print('Error deleting subprocess: $e');
+  Future<List<SubProcess>> getByProcessId(int id) async {
+    await subprocessRepository.syncSubProcess();
+    return await subprocessRepository.getByProcessId(id);
   }
-}
 
-Future<List<SubProcess>> getByStatusAndUserId(int status, int userid) async {
-  return await subprocessRepository.getByStatusAndUserId(status, userid);
-}
+  Future<List<SubProcess>> getByUserId(int id) async {
+    await subprocessRepository.syncSubProcess();
+    return await subprocessRepository.getByUserId(id);
+  }
 
+  Future<List<SubProcess>> getByUserAndProcess(
+      int userId, int processId) async {
+        await subprocessRepository.syncSubProcess();
+    return await subprocessRepository.getByUserAndProcessId(userId, processId);
+  }
+
+  Future<void> update(SubProcess subProcess) async {
+    try {
+      subprocess = await subprocessRepository.updateSubProcess(subProcess);
+    } catch (e) {
+      print('Error updating subprocess: $e');
+    }
+  }
+
+  Future<void> delete(int id) async {
+    try {
+      await subprocessRepository.deleteSubProcess(id);
+    } catch (e) {
+      print('Error deleting subprocess: $e');
+    }
+  }
+
+  Future<List<SubProcess>> getByStatusAndUserId(int status, int userid) async {
+    await subprocessRepository.syncSubProcess();
+    return await subprocessRepository.getByStatusAndUserId(status, userid);
+  }
 }
